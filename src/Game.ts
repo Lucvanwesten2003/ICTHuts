9286fa4e20cbc736716dfbf6163e3e751fca177f
vincenthollestelle <70520349+vincenthollestelle@users.noreply.github.com>
2021-01-07 12:40:22 +0100

    f5

--- src/Game.ts
+++ src/Game.ts
@@ -61,7 +61,7 @@ class Game {
             location.reload()
         }
         if (this.keyBoard.isKeyDown(KeyboardListener.KEY_F5)) {
-            window.location.href = 'index.html'
+            location.href = 'index.html'
         }
         requestAnimationFrame(this.loop);
     };
