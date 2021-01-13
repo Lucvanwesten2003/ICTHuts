60da172065dd197ee7bc1d41aa3e695e6b5adb77
Rohan Phaff <2505rohan@gmail.com>
2021-01-13 14:16:03 +0100

    De game werk helemaal (FILIP! GODVERDOMME)

--- build/app.js
+++ build/app.js
@@ -1,3 +1,39 @@
+class CanvasElements {
+    constructor(name, xPos, yPos, image) {
+        this.name = name;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.image = this.loadNewImage(image);
+    }
+    getName() {
+        return this.name;
+    }
+    getXPos() {
+        return this.xPos;
+    }
+    getYPos() {
+        return this.yPos;
+    }
+    getImageWidth() {
+        return this.image.width;
+    }
+    getImageHeight() {
+        return this.image.height;
+    }
+    draw(ctx) {
+        ctx.drawImage(this.image, this.xPos, this.yPos);
+    }
+    loadNewImage(source) {
+        const img = new Image();
+        img.src = source;
+        return img;
+    }
+}
+class CanvasElement extends CanvasElements {
+    constructor(name, xPos, yPos, image) {
+        super(name, xPos, yPos, image);
+    }
+}
 class Portal {
     constructor(image) {
         this._image = this.loadNewImage(image);
@@ -20,57 +56,147 @@ class EndPortal extends Portal {
 class Game {
     constructor(canvasId) {
         this.loop = () => {
-            this.newLevel();
-            this.score++;
-            this.counter++;
-            if (this.counter === 60) {
-                this.makeFish();
-                this.counter = 0;
-            }
-            console.log(this.level);
-            this.draw();
-            this.move();
-            this.drawHengel(this.ctx);
-            this.drawPortal(this.ctx);
-            this.hengel.move(this.canvas);
-            this.hengel.hengelCollidesWithFish(this.rockets, this.player);
-            this.player.move(this.canvas);
-            if (this.keyBoard.isKeyDown(KeyboardListener.KEY_F11)) {
-                location.reload();
-            }
-            if (this.keyBoard.isKeyDown(KeyboardListener.KEY_F5)) {
-                window.location.href = 'https://lucvanwesten2003.github.io/ICTHuts/';
+            if (this.shop === false) {
+                this.newLevel();
+                this.score++;
+                this.counter++;
+                if (this.counter === 60) {
+                    this.makeFish();
+                    this.counter = 0;
+                }
+                this.move();
+                this.draw();
+                this.drawHengel(this.ctx);
+                this.drawPortal(this.ctx);
+                this.hengel.move(this.canvas);
+                this.hengel.hengelCollidesWithFish(this.rockets, this.player, this.double);
+                this.player.move(this.canvas);
+                if (this.keyBoard.isKeyDown(KeyboardListener.KEY_F11)) {
+                    location.reload();
+                }
+                if (this.keyBoard.isKeyDown(KeyboardListener.KEY_F5)) {
+                    window.location.href = 'https://lucvanwesten2003.github.io/ICTHuts/';
+                }
             }
             requestAnimationFrame(this.loop);
+        };
+        this.mouseHandler = (event) => {
+            let X = event.clientX / window.innerWidth;
+            let Y = event.clientY / window.innerHeight;
+            console.log(X);
+            console.log(Y);
+            if (X > 0 && X < 0.135 && Y > 0.138 && Y < 0.416) {
+                this.shop = true;
+                document.body.style.background = `url("./assets/Images/The Shop.png") no-repeat center center fixed`;
+                document.body.style.backgroundSize = 'cover';
+            }
+            if (this.shop === true) {
+                if (X > 0.829 && X < 0.964 && Y > 0.051 && Y < 0.312) {
+                    this.Prompts.splice(0, 1);
+                    this.shop = false;
+                }
+                if (this.twoXPopup === false && this.specialfishPopup === false && X > 0.321 && X < 0.430 && Y > 0.644 && Y < 0.722 || this.speedPopup === true) {
+                    this.Prompts.splice(0, 1);
+                    this.Prompts.push(new CanvasElement("Potion prompt", 0, -200, "./assets/Images/SpeedPrompt.png"));
+                    this.speedPopup = true;
+                    if (X > 0.439 && X < 0.473 && Y > 0.368 && Y < 0.434 && this.speedPotion === false && this.hengel._score >= 10) {
+                        this.hengel._score = this.hengel._score - 10;
+                        this.speedPotion = true;
+                        this.player.speed = this.player.speed * 2;
+                        this.Powerups.push(new CanvasElement("Speed Power", 0, 0, "./assets/Images/SpeedPower.png"));
+                        this.Prompts.splice(0, 1);
+                        this.speedPopup = false;
+                    }
+                    if (X > 0.516 && X < 0.551 && Y > 0.370 && Y < 0.432) {
+                        console.log("no");
+                        this.Prompts.splice(0, 1);
+                        this.speedPopup = false;
+                    }
+                }
+                if (this.speedPopup === false && this.specialfishPopup === false && X > 0.445 && X < 0.552 && Y > 0.643 && Y < 724 || this.twoXPopup === true) {
+                    this.Prompts.splice(0, 1);
+                    this.Prompts.push(new CanvasElement("two X Prompt", 0, -200, "./assets/Images/DoublePrompt.png"));
+                    this.twoXPopup = true;
+                    if (X > 0.441 && X < 0.475 && Y > 0.371 && Y < 0.436 && this.double === false && this.hengel._score >= 15) {
+                        this.hengel._score = this.hengel._score - 15;
+                        this.double = true;
+                        this.Powerups.push(new CanvasElement("Double Points Power", 0, 0, "./assets/Images/DoublePointsPower.png"));
+                        this.Prompts.splice(0, 1);
+                        this.twoXPopup = false;
+                    }
+                    if (X > 0.518 && X < 0.553 && Y > 0.371 && Y < 0.432) {
+                        console.log("no");
+                        this.Prompts.splice(0, 1);
+                        this.twoXPopup = false;
+                    }
+                }
+                if (this.speedPopup === false && this.twoXPopup === false && X > 0.574 && X < 0.681 && Y > 0.644 && Y < 0.721 || this.specialfishPopup === true) {
+                    this.Prompts.splice(0, 1);
+                    this.Prompts.push(new CanvasElement("Special Fish Prompt", 0, -200, "./assets/Images/SpecialPrompt.png"));
+                    this.specialfishPopup = true;
+                    if (X > 0.442 && X < 0.471 && Y > 0.362 && Y < 0.421 && this.special === false && this.hengel._score >= 20) {
+                        this.hengel._score = this.hengel._score - 20;
+                        this.special = true;
+                        this.Powerups.push(new CanvasElement("Special Fish Power", 0, 0, "./assets/Images/SpecialFishPower.png"));
+                        this.Prompts.splice(0, 1);
+                        this.specialfishPopup = false;
+                    }
+                    if (X > 0.511 && X < 0.541 && Y > 0.360 && Y < 0.416) {
+                        console.log("no");
+                        this.Prompts.splice(0, 1);
+                        this.specialfishPopup = false;
+                    }
+                }
+            }
+            this.draw();
         };
         this.canvas = canvasId;
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext("2d");
         this.rockets = [];
+        this.Prompts = [];
+        this.Powerups = [];
+        this.speedPopup = false;
+        this.twoXPopup = false;
+        this.specialfishPopup = false;
+        this.shop = false;
+        this.double = false;
+        this.special = false;
+        this.speedPotion = false;
         this.keyBoard = new KeyboardListener;
+        document.addEventListener("click", this.mouseHandler);
         console.log(this.rockets);
         this.player = new Player('Me', this.canvas.width / 2.25, this.canvas.height / 2 - 80, 5, "./assets/Images/mcboot.png");
         console.log(this.player);
         this.hengel = new Hengel(this.canvas.height / 2 - 60, 3, "./assets/Images/hook.png");
         this.netherPortal = new NetherPortal("./assets/Images/nether_portal.png");
         this.endPortal = new EndPortal("./assets/Images/end_portal.png");
-        this.soundEffect("./assets/Sounds/Background_music.mp3", 0.5, 0.3);
+        this.soundEffect("./assets/Sounds/Background_music.mp3", 0.5, 0.05);
         this.score = 0;
-        this.level = 0;
+        this.level = 1;
         this.loop();
         this.counter = 0;
     }
     makeFish() {
         for (let index = 0; index < 1; index++) {
-            let randomFish = ['alive', 'dead'];
+            let randomFish;
+            if (this.special === false) {
+                randomFish = ['alive', 'dead'];
+            }
+            if (this.special === true) {
+                randomFish = ['alive', 'dead', 'special'];
+            }
             const randomElement = randomFish[Math.floor(Math.random() * randomFish.length)];
             if (randomElement === 'alive') {
                 this.rockets.push(new Rocket('aliveFish', Game.randomNumber(0, this.canvas.width - 200), Game.randomNumber(this.player.yPosition + 200, this.canvas.height - 50), Game.randomNumber(2, 5), "aliveFish", "./assets/Images/aliveFish.png"));
                 console.log("alvieFish");
             }
-            else {
+            if (randomElement === 'dead') {
                 this.rockets.push(new Rocket('deadFish', Game.randomNumber(0, this.canvas.width - 200), Game.randomNumber(this.player.yPosition + 200, this.canvas.height - 50), Game.randomNumber(2, 5), "deadFish", "./assets/Images/deadFish.png"));
+            }
+            if (randomElement === 'special') {
+                this.rockets.push(new Rocket('specialFish', Game.randomNumber(0, this.canvas.width - 200), Game.randomNumber(this.player.yPosition + 200, this.canvas.height - 50), Game.randomNumber(2, 5), "SpecialFish", "./assets/Images/specialFish.png"));
             }
         }
     }
@@ -89,33 +215,55 @@ class Game {
         ctx.stroke();
     }
     drawPortal(ctx) {
-        if (this.level == 1) {
-            if (this.hengel._score > 2) {
-                ctx.drawImage(this.netherPortal.image, this.canvas.width - this.netherPortal.image.width, this.player.yPosition - 100);
-                this.portalCollision();
+        if (this.shop === false) {
+            if (this.level === 1) {
+                if (this.hengel._score >= 25) {
+                    ctx.drawImage(this.netherPortal.image, this.canvas.width - this.netherPortal.image.width, this.player.yPosition - 100);
+                    this.portalCollision();
+                }
             }
-        }
-        if (this.level == 2) {
-            if (this.hengel._score > 7) {
-                ctx.drawImage(this.endPortal.image, this.canvas.width - this.endPortal.image.width, this.player.yPosition - 120);
-                this.portalCollision();
+            if (this.level === 2) {
+                if (this.hengel._score >= 75) {
+                    ctx.drawImage(this.endPortal.image, this.canvas.width - this.endPortal.image.width, this.player.yPosition - 120);
+                    this.portalCollision();
+                }
             }
         }
     }
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.player.draw(this.ctx);
-        if (this.rockets.length != 0) {
-            this.rockets.forEach((rocket) => {
-                rocket.draw(this.ctx);
+        if (this.shop === true) {
+            this.Prompts.forEach((element) => {
+                element.draw(this.ctx);
             });
-            this.writeTextToCanvas(this.ctx, `Score is: ${this.hengel._score}`, 40, this.canvas.width / 2, 40);
+        }
+        this.writeTextToCanvas(this.ctx, `Score is: ${this.hengel._score}`, 40, this.canvas.width / 2, 40);
+        if (this.shop === true) {
+            this.Powerups.forEach((element) => {
+                element.draw(this.ctx);
+            });
+        }
+        if (this.shop === false) {
+            this.player.draw(this.ctx);
+            if (this.rockets.length != 0) {
+                this.rockets.forEach((rocket) => {
+                    rocket.draw(this.ctx);
+                });
+            }
         }
     }
     newLevel() {
-        if (this.hengel._score < 3) {
+        if (this.level === 1) {
             this.level = 1;
             document.body.style.background = `url("./assets/Images/achtergrond_level_1.png") no-repeat center center fixed`;
+            document.body.style.backgroundSize = 'cover';
+        }
+        if (this.level === 2) {
+            document.body.style.background = `url("./assets/Images/achtergrond_level_2.png") no-repeat center center fixed`;
+            document.body.style.backgroundSize = 'cover';
+        }
+        if (this.level === 3) {
+            document.body.style.background = `url("./assets/Images/achtergrond_level_3.png") no-repeat center center fixed`;
             document.body.style.backgroundSize = 'cover';
         }
     }
@@ -127,6 +275,7 @@ class Game {
             document.body.style.background = `url("./assets/Images/achtergrond_level_2.png") no-repeat center center fixed`;
             document.body.style.backgroundSize = 'cover';
             this.player.xPosition = 0;
+            this.hengel._score = 0;
             console.log("next level");
         }
         if (this.player.xPosition >= this.canvas.width - this.endPortal.image.width - this.player.image.width && this.level == 2) {
@@ -135,6 +284,7 @@ class Game {
             document.body.style.background = `url("./assets/Images/achtergrond_level_3.png") no-repeat center center fixed`;
             document.body.style.backgroundSize = 'cover';
             this.player.xPosition = 0;
+            this.hengel._score = 0;
             console.log("next level");
         }
     }
@@ -274,8 +424,7 @@ class Hengel {
         img.src = source;
         return img;
     }
-    hengelCollidesWithFish(rockets, player) {
-        console.log(this.maxY);
+    hengelCollidesWithFish(rockets, player, double) {
         rockets.forEach((rocket, index) => {
             if (rocket.yPosition <= this.maxY) {
                 rockets.splice(index, 1);
@@ -294,21 +443,38 @@ class Hengel {
             }
         });
         if (this.checker === 1) {
-            this.updatePosition(this.catchedFish, this.player);
+            this.updatePosition(this.catchedFish, this.player, double);
         }
     }
-    updatePosition(rocket, player) {
+    updatePosition(rocket, player, double) {
         rocket.yPosition = this._yPosition;
         rocket.xPosition = player.xPosition + player.image.width - 50;
         console.log(rocket.yPosition);
         if (rocket.yPosition <= this.maxY && rocket._name == "aliveFish") {
-            this.score++;
+            if (double === true) {
+                this.score++;
+                this.score++;
+            }
+            if (double === false) {
+                this.score++;
+            }
             this.checker = 0;
             this.soundEffect("./assets/Sounds/good_fish.mp3", 1.2, 0.3);
         }
         if (rocket.yPosition <= this.maxY && rocket._name == "deadFish") {
             this.checker = 0;
             this.soundEffect("./assets/Sounds/oof_sound.mp3", 0.5, 0.5);
+        }
+        if (rocket.yPosition <= this.maxY && rocket._name == "specialFish") {
+            if (double === true) {
+                this.score++;
+                this.score++;
+            }
+            if (double === false) {
+                this.score++;
+            }
+            this.checker = 0;
+            this.soundEffect("./assets/Sounds/good_fish.mp3", 1, 0.3);
         }
     }
     soundEffect(url, time, volume) {
@@ -319,6 +485,9 @@ class Hengel {
     }
     get _score() {
         return this.score;
+    }
+    set _score(score) {
+        this.score = score;
     }
 }
 class KeyboardListener {
---
+++ src/CanvasElement.ts
@@ -0,0 +1,12 @@
+/// <reference path="CanvasElements.ts"/>
+
+class CanvasElement extends CanvasElements{
+    constructor(
+        name: string,
+        xPos: number,
+        yPos: number,
+        image: string,
+    ) {
+        super(name, xPos, yPos, image);
+    }
+}
